e88632b7a616e53de6a1272c498d7e49acd7f536
React_Learn <user.email:"gopalkrishnanto@email.com>
2025-06-16 09:24:05 +0530

    This is the my React learning

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+// import "./index.scss";
+// import { Header, Header2 } from "./Header";
+// import Props from "./props";
+// import { ColorBox } from "./ColorBox";
+
+// const App = () => {
+
+  
+//   // Function to handle button click
+//   const handleClick = () => {
+//     alert("Button clicked!");
+//   };
+
+//   return (
+//     <div className="container mt-5 text-center">
+//       <h1 className="text-primary">Hello, World!</h1>
+//       <p className="lead">
+//         This is a simple React + Bootstrap + Sass application.
+//       </p>
+//       <button className="btn btn-success" onClick={handleClick}>
+//         Click Me
+//       </button>
+//     </div>
+//   );
+// };
+
+// const App5 = () => {
+//   return (
+//     <div className="container mt-5">
+//       <h2>ColorBox Example</h2>
+//       <ColorBox color="red" />
+//       <ColorBox color="blue" />
+//       <ColorBox color="yellow" />
+//       {/* <ColorBox color="yellow" />  <-- This will show a warning in console! */}
+//     </div>
+//   );
+// };
+
+// const App2 = () => <Header />;
+// const App3 = () => <Header2 />;
+// const App4 = () => <Props />;
+
+// export { App, App2, App3, App4 ,App5};
---
+++ src/main.jsx
@@ -0,0 +1,69 @@
+// {
+//   /*import React from "react";
+// import ReactDOM from "react-dom/client";
+// import {Card} from "./Card";
+// import { Qr_code } from "./components/Qr_code";
+// import {Qr} from "./components/Qr_code"
+// import { App, App2, App3, App4,App5 } from "./App";
+// import List from "./List";
+// ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+
+// ReactDOM.createRoot(document.getElementById("root1")).render(<App2 />);
+
+// ReactDOM.createRoot(document.getElementById("root2")).render(<App3 />);
+
+// ReactDOM.createRoot(document.getElementById("root3")).render(<List />);
+
+// ReactDOM.createRoot(document.getElementById("root4")).render(<App4 />);
+// ReactDOM.createRoot(document.getElementById("root5")).render(
+//   <React.StrictMode>
+//     <App5 />
+//   </React.StrictMode>
+// );
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//     <Card />
+// ); */
+// }
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import Qr_code from "./components/Qr_code/Qr_code";
+
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//   <React.StrictMode>
+//     <Qr_code />
+//   </React.StrictMode>
+// );
+
+// console.log("hi");
+
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import Qr_code from "./components/QR_Code_Tutore/Qr_Code";
+
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//   <React.StrictMode>
+//     <Qr_code />
+//   </React.StrictMode>
+// );
+import React from "react";
+import ReactDOM from "react-dom/client";
+// import { UseState } from "./components/UseState/UseState";
+
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//   <React.StrictMode>
+//     <UseState />
+//   </React.StrictMode>
+// );
+// import { FormData } from "./components/Form/FormData";
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//   <React.StrictMode>
+//     <FormData />
+//   </React.StrictMode>
+// );
+
+import { FormData1 } from "./components/Form/FormData";
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <FormData1 />
+  </React.StrictMode>
+);
